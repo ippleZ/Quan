e2bf5f9919655d1246762c3f92aeceeb48aa2687
ippleX <63136760+ippleX@users.noreply.github.com>
2023-05-23 20:43:29 +0800

    Update expedite.js

--- other/expedite.js
+++ other/expedite.js
@@ -4,7 +4,7 @@ var regex2 = /https:\/\/raw\.githubusercontent\.com\/(.+)\/(.+)\/(.+)\/(.+)/gmi;
 
 // 替换匹配项
 var modifiedBody = $response.body.replace(regex1, 'https://raw.gitmirror.com/$1/$2/$3/$4');
-modifiedBody = modifiedBody.replace(regex2, 'https://raw.gitmirror.com/$1//$2/$3/$4');
+modifiedBody = modifiedBody.replace(regex2, 'https://raw.gitmirror.com/$1/$2/$3/$4');
 
 // 返回修改后的响应体
 $done({ body: modifiedBody })
