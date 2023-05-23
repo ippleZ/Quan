13abdc169f3be4c4f4ed90ac91b2c80f2c3a1c90
ippleX <63136760+ippleX@users.noreply.github.com>
2023-05-23 20:24:14 +0800

    Update expedite.js

--- other/expedite.js
+++ other/expedite.js
@@ -3,9 +3,8 @@ var regex1 = /https:\/\/github\.com\/ddgksf2013\/Scripts\/raw\/master\/(.+)/gmi;
 var regex2 = /https:\/\/raw.githubusercontent.com\/(.+)/gmi;
 
 // 替换匹配项
-var modifiedBody = $response.body.replace(regex2, 'https://raw.gitmirror.com/$1');
-// (regex1, 'https://raw.gitmirror.com/ddgksf2013/Scripts/master/$1');
-// modifiedBody = modifiedBody.replace
+var modifiedBody = $response.body.replace(regex1, 'https://raw.gitmirror.com/ddgksf2013/Scripts/master/$1');
+modifiedBody = modifiedBody.replace(regex2, 'https://raw.gitmirror.com/$1');
 
 // 返回修改后的响应体
 $done({ body: modifiedBody })
