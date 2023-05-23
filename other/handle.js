54570825c38cfca7e739115fd0c400ecad8c08f6
ippleX <63136760+ippleX@users.noreply.github.com>
2023-05-23 21:58:01 +0800

    Update handle.js

--- other/handle.js
+++ other/handle.js
@@ -1,8 +1,10 @@
 // 定义正则表达式模式
-var regex1 = /https:\/\/github\.com\/ddgksf2013\/Scripts\/raw\/master\/(.+)/gmi;
+var regex1 = /https:\/\/github\.com\/(.+?)\/(.+?)\/raw\/(.+?)\/(.+)/gmi;
+var regex2 = /https:\/\/raw\.githubusercontent\.com\/(.+?)\/(.+?)\/(.+?)\/(.+)/;
 
 // 替换匹配项
-var modifiedBody = $response.body.replace(regex1, 'https://raw.gitmirror.com/ddgksf2013/Scripts/master/$1');
+var modifiedBody = $response.body.replace(regex1, 'https://raw.gitmirror.com/$1/$2/$3/$4');
+modifiedBody = modifiedBody.replace(regex2, 'https://raw.gitmirror.com/$1/$2/$3/$4');
 
 // 返回修改后的响应体
-$done({ body: modifiedBody })
+$done({ body: modifiedBody });
