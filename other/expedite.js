5ad3dbd0807c4b30c8b9d14cac5732f076dfa380
ippleX <63136760+ippleX@users.noreply.github.com>
2023-05-23 20:37:45 +0800

    Update expedite.js

--- other/expedite.js
+++ other/expedite.js
@@ -1,9 +1,9 @@
 // 定义正则表达式模式
-var regex1 = /https:\/\/github\.com\/ddgksf2013\/Scripts\/raw\/master\/(.+)/gmi;
+var regex1 = /https:\/\/github\.com\/(.+)\/(.+)\/raw\/(.+)\/(.+)/gmi;
 var regex2 = /https:\/\/raw\.githubusercontent\.com\/(.+)/gmi;
 
 // 替换匹配项
-var modifiedBody = $response.body.replace(regex1, 'https://raw.gitmirror.com/ddgksf2013/Scripts/master/$1');
+var modifiedBody = $response.body.replace(regex1, 'https://raw.gitmirror.com/$1/$2/$3/$4');
 modifiedBody = modifiedBody.replace(regex2, 'https://raw.gitmirror.com/$1');
 
 // 返回修改后的响应体
