664b6311e1355d262879f3d0537a51884c81c043
橙子 <134483956+ippleZ@users.noreply.github.com>
2023-05-25 22:20:48 +0800

    Delete 1.js

--- filter/1.js
+++
@@ -1,5 +0,0 @@
-var emojiRegex = /[\uD800-\uDBFF][\uDC00-\uDFFF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDEFF]/g;
-
-var modifiedBody = $response.body.replace(emojiRegex, '');
-
-$done({ body: modifiedBody });
