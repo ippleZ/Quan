c75565562cd7bd63c35fb9804248fa80726fac17
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-06-23 13:16:00 +0800

    Update and rename handle.js to expedite-raw.js

--- other/expedite-raw.js
+++ other/expedite-raw.js
@@ -3,7 +3,7 @@ var regex1 = /^https?:\/\/github\.com\/(.+?)\/(.+?)\/raw\/(.+?)\/(.+)/gmi;
 var regex2 = /^https?:\/\/raw\.githubusercontent\.com\/(.+?)\/(.+?)\/(.+?)\/(.+)/gmi;
 
 // 替换匹配项
-var modifiedBody = $response.body.replace(regex1, 'https://raw.gitmirror.com/$1/$2/$3/$4');
+var modifiedBody = $response.body。replace(regex1, 'https://raw.gitmirror.com/$1/$2/$3/$4');
 modifiedBody = modifiedBody.replace(regex2, 'https://raw.gitmirror.com/$1/$2/$3/$4');
 
 // 返回修改后的响应体
