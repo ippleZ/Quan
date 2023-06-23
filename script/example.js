a3dc0858d73471f4f523c6598837f5831971a466
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-06-23 13:50:06 +0800

    Update example.js

--- script/example.js
+++ script/example.js
@@ -1,3 +1,5 @@
-var body = $response.body
-  .replace(/<head>/, '<head><link rel="stylesheet" href="https://example/example.css" type="text/css">');
+let ele = '<head>';
+let eleReplace = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" /><script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
+let body = $response.body
+    .replace(ele, eleReplace)
 $done({ body });
