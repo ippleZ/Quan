e9c690c5bb4b00484d0937b11d68a8538eff8f9b
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-06-28 20:38:18 +0800

    Update expedite.js

--- other/expedite.js
+++ other/expedite.js
@@ -1,12 +1,12 @@
 // 定义正则表达式模式
-var regex1 = /^https:\/\/github\.com\/(.+?)\/(.+?)\/raw\/(.+?)\/(.+)/gmi;
-var regex2 = /^https:\/\/raw\.githubusercontent\.com\/(.+?)\/(.+?)\/(.+?)\/(.+)/gmi;
+var regex1 = https:\/\/github\.com\/(.+?)\/(.+?)\/raw\/(.+?)\/(.+)/gmi;
+var regex2 = https:\/\/raw\.githubusercontent\.com\/(.+?)\/(.+?)\/(.+?)\/(.+)/gmi;
 
 // 替换匹配项
-var modifiedBody = $response.body.replace(regex1, function(match, p1, p2, p3, p4) {
+var modifiedBody = $response.body。replace(regex1, function(match， p1， p2， p3， p4) {
     return 'https://raw.gitmirror.com/' + p1 + '/' + p2 + '/' + p3 + '/' + p4;
 });
-modifiedBody = modifiedBody.replace(regex2, function(match, p1, p2, p3, p4) {
+modifiedBody = modifiedBody.replace(regex2, function(match， p1， p2， p3， p4) {
     return 'https://raw.gitmirror.com/' + p1 + '/' + p2 + '/' + p3 + '/' + p4;
 });
 
