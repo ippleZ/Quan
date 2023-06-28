b476e5233fa5eb1cea1ad1d5d6d07d136e72bcb3
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-06-28 21:57:18 +0800

    Update expedite.js

--- other/expedite.js
+++ other/expedite.js
@@ -1,6 +1,6 @@
 // 定义正则表达式模式
-var regex1 = /https:\/\/github\.com\/(.+?)\/(.+?)\/raw\/(.+?)\/(.+)/gi;
-var regex2 = /https:\/\/raw\.githubusercontent\.com\/(.+?)\/(.+?)\/(.+?)\/(.+)/gi;
+var regex1 = /https:\/\/github\.com\/(.+)\/(.+)\/raw\/(.+)\/(.+)/gi;
+var regex2 = /https:\/\/raw\.githubusercontent\.com\/(.+)\/(.+)\/(.+)\/(.+)/gi;
 
 // 替换匹配项
 var modifiedBody = $response.body.replace(regex1, function(match, p1, p2, p3, p4) {
