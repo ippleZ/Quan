4d9f6003c8144264acc28289f21dab16b2939533
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-06-28 20:44:35 +0800

    Update expedite.js

--- other/expedite.js
+++ other/expedite.js
@@ -3,10 +3,10 @@ var regex1 = https:\/\/github\.com\/(.+?)\/(.+?)\/raw\/(.+?)\/(.+)/gmi;
 var regex2 = https:\/\/raw\.githubusercontent\.com\/(.+?)\/(.+?)\/(.+?)\/(.+)/gmi;
 
 // 替换匹配项
-var modifiedBody = $response.body.replace(regex1, function(match， p1， p2， p3， p4) {
+var modifiedBody = $response.body。replace(regex1, function(match, p1, p2, p3, p4) {
     return 'https://raw.gitmirror.com/' + p1 + '/' + p2 + '/' + p3 + '/' + p4;
 });
-modifiedBody = modifiedBody.replace(regex2, function(match， p1， p2， p3， p4) {
+modifiedBody = modifiedBody.replace(regex2, function(match, p1, p2, p3, p4) {
     return 'https://raw.gitmirror.com/' + p1 + '/' + p2 + '/' + p3 + '/' + p4;
 });
 
