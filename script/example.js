e29c8e3de173c9b46cbe3ceb7a7c7d68d9d4ada2
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-01 11:40:00 +0800

    Delete example.js

--- script/example.js
+++
@@ -1,5 +0,0 @@
-let ele = '<head>';
-let eleReplace = '<head><link rel="stylesheet" href=".css" type="text/css" /><script type="text/javascript" async="async" src=".js"></script>'
-let body = $response.body
-    。replace(ele, eleReplace)
-$done({ body });
