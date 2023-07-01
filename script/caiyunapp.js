cdef956e37ece45840a3797bed0eb35f84faccef
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-01 21:46:52 +0800

    Update caiyunapp.js

--- script/caiyunapp.js
+++ script/caiyunapp.js
@@ -1,13 +1,13 @@
 var url = $request.url;
 var obj = JSON.parse($response.body);
 let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
+
 if (url.indexOf('/user') != -1 || url.indexOf('/visitors') != -1) {
 	obj.result.token = Token;
-	body = JSON.stringify(obj);
 }
 
 if (url.indexOf('/login_by_code') != -1) {
-	let obj = {
+	obj = {
 		"status": "ok",
 		"result": {
 			"is_phone_verified": true,
@@ -15,8 +15,17 @@ if (url.indexOf('/login_by_code') != -1) {
 		},
 		"rc": 0
 	}
-	body = JSON.stringify(obj);
 }
-$done({
-	body
-});
+
+obj.result = {
+	"name": "十点半睡觉",
+	"avatar": https://cdn.jsdelivr.net/gh/Koolson/Qure/IconSet/mini/Quantumult_X.png
+};
+obj.weixin = {
+	"id": "",
+	"username": "",
+	"is_bound": false
+};
+var body = JSON.stringify(obj);
+
+$done({ body });
