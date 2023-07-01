04c1e84da85243286d81e89d514f91c8da55b5c1
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-02 00:36:10 +0800

    更新 caiyunapp.js

--- script/caiyunapp.js
+++ script/caiyunapp.js
@@ -1,13 +1,13 @@
 var url = $request.url;
 var obj = JSON.parse($response.body);
 let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
-
 if (url.indexOf('/user') != -1 || url.indexOf('/visitors') != -1) {
-	obj.result.token = Token;
+		obj.result.token = Token;
+	body = JSON.stringify(obj);
 }
 
 if (url.indexOf('/login_by_code') != -1) {
-	obj = {
+	let obj = {
 		"status": "ok",
 		"result": {
 			"is_phone_verified": true,
@@ -15,8 +15,6 @@ if (url.indexOf('/login_by_code') != -1) {
 		},
 		"rc": 0
 	}
+	body = JSON.stringify(obj);
 }
-
-var body = JSON.stringify(obj);
-
-$done({ body });
+$done({body});
