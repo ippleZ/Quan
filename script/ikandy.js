484dae526a834aa0b215ccaf823260fccf62dd13
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-01 12:30:02 +0800

    Update ikandy.js

--- script/ikandy.js
+++ script/ikandy.js
@@ -1,4 +1,4 @@
 var modifiedBody = $response.body
-	.replace(/(position:absolute;right:0;top:0px;width:)\d+px(;height:)\d+px(;text-align:center)/g, "$10px$20px$30px")
-	.replace(/(position: fixed;top: 60px;right:0;z-index:999;width:)\d+px/g, "$10px")
-$done({body: modifiedBody});
+	.replace('<img id="tj" onclick="window.open(\'/topicdetail-9\')" src="${tj_pic}" width="100%" >', '<img id="tj" onclick="window.open(\'/topicdetail-9\')" src="${tj_pic}" width="0%" >')
+	.replace('<div class="marquee_outer">', '<div class="marquee_outer" style="display: none">');
+$done({ body: modifiedBody });
