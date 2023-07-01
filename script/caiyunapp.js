50bd742a7eedd5a9ec0fd019b393e9c69118a084
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-02 00:46:24 +0800

    更新 caiyunapp.js

--- script/caiyunapp.js
+++ script/caiyunapp.js
@@ -3,10 +3,6 @@ var obj = JSON.parse($response.body);
 let Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
 if (url.indexOf('/user') != -1 || url.indexOf('/visitors') != -1) {
     obj.result.token = Token;
-    obj.result = {
-        "name": "十点半睡觉",
-        "avatar": "https://cdn.jsdelivr.net/gh/Koolson/Qure/IconSet/mini/Quantumult_X.png"
-    };
     body = JSON.stringify(obj);
 }
 
