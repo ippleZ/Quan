340fd82574ea47fc6d63ffc7ee93091f4f609f13
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-02 12:14:51 +0800

    Delete 555.js

--- script/555.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.gitmirror.com/ippleZ/Quan/master/script/555.css" type="text/css">');
-$done({ body });
