4782fb8ccea677d4a9c31822f9b63a6f451d848d
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-02 20:24:10 +0800

    Update ikan.js

--- script/ikan.js
+++ script/ikan.js
@@ -2,7 +2,7 @@ var body = $response.body
   .replace(/position\s*:\s*fixed\s*;\s*top\s*:\s*60px\s*;\s*right\s*:\s*0\s*;\s*z-index\s*:\s*999\s*;\s*width\s*:\s*250px/, 'position: fixed;top: 60px;right:0;z-index:999;width:0px')
   .replace(/position\s*:\s*absolute\s*;\s*right\s*:\s*0\s*;\s*top\s*:\s*0px\s*;\s*width\s*:\s*33px\s*;\s*height\s*:\s*33px\s*;\s*text-align\s*:\s*center/, 'position:absolute;right:0;top:0px;width:0px;height:0px;text-align:center')
   .replace(/src="\$\{tj_pic\}" width="100%"/g, 'src="${tj_pic}" width="0%"')
-  .replace(/class="marquee_outer"/, 'class="marquee_outer" style="display:none"')
+  .replace(/class="marquee_outer"/, 'class="marquee_outer" style="display:none !important"')
   .replace(/class="stui-pannel stui-pannel-bg clearfix" style="margin-bottom\s*:\s*0"/g, 'class="stui-pannel stui-pannel-bg clearfix" style="margin-bottom:0;display:none"')
   .replace(/159140/g, '990929');
 $done({ body });
