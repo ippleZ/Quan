39d0a10dfee1642e3c910f8bd964e416be388d41
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-02 13:03:30 +0800

    Update ikan.js

--- script/ikan.js
+++ script/ikan.js
@@ -1,1 +1,3 @@
-.replace(/position\s*:\s*fixed\s*;\s*top\s*:\s*60px\s*;\s*right\s*:\s*0\s*;\s*z-index\s*:\s*999\s*;\s*width\s*:\s*250px/, 'position: fixed;top: 60px;right:0;z-index:999;width:0px')
+var body = $response.body
+  .replace(/position\s*:\s*fixed\s*;\s*top\s*:\s*60px\s*;\s*right\s*:\s*0\s*;\s*z-index\s*:\s*999\s*;\s*width\s*:\s*250px/, 'position: fixed;top: 60px;right:0;z-index:999;width:0px');
+$done({ body });
