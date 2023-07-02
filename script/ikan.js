90d56cf9b8197da8d4931246d0e6e5abff846583
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-02 13:31:33 +0800

    Update ikan.js

--- script/ikan.js
+++ script/ikan.js
@@ -1,6 +1,6 @@
 var body = $response.body
   .replace(/position\s*:\s*fixed\s*;\s*top\s*:\s*60px\s*;\s*right\s*:\s*0\s*;\s*z-index\s*:\s*999\s*;\s*width\s*:\s*250px/, 'position: fixed;top: 60px;right:0;z-index:999;width:0px')
   .replace(/position\s*:\s*absolute\s*;\s*right\s*:\s*0\s*;\s*top\s*:\s*0px\s*;\s*width\s*:\s*33px\s*;\s*height\s*:\s*33px\s*;\s*text-align\s*:\s*center/, 'position:absolute;right:0;top:0px;width:0px;height:0px;text-align:center')
-  .replace(/src="https:\/\/sp\.fp\.ps\.netease\.com\/.*"\s*width="100%"/g, 'width="0%"')
-  .replace(/class="marquee_outer"/g, 'class="marquee_outer" style="display:none"');
+  .replace(/src="https:\/\/sp\.fp\.ps\.netease\.com\/.*"\s*width="100%"/, 'width="0%"')
+  .replace(/class="marquee_outer"/, 'class="marquee_outer" style="display:none"');
 $done({ body });
