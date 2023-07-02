655b8dc6759a069d364c85a2782627a634a5a0f0
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-02 14:01:01 +0800

    Update ikan.js

--- script/ikan.js
+++ script/ikan.js
@@ -3,5 +3,6 @@ var body = $response.body
   .replace(/position\s*:\s*absolute\s*;\s*right\s*:\s*0\s*;\s*top\s*:\s*0px\s*;\s*width\s*:\s*33px\s*;\s*height\s*:\s*33px\s*;\s*text-align\s*:\s*center/, 'position:absolute;right:0;top:0px;width:0px;height:0px;text-align:center')
   .replace(/src="https:\/\/sp\.fp\.ps\.netease\.com\/.*"\s*width="100%"/, 'width="0%"')
   .replace(/src="\$\{tj_pic\}" width="100%"/g, 'src="${tj_pic}" width="0%"')
-  .replace(/class="stui-pannel stui-pannel-bg clearfix" style="margin-bottom\s*:\s*0"/g, 'class="stui-pannel stui-pannel-bg clearfix" style="margin-bottom:0;display:none"');
+  .replace(/class="stui-pannel stui-pannel-bg clearfix" style="margin-bottom\s*:\s*0"/g, 'class="stui-pannel stui-pannel-bg clearfix" style="margin-bottom:0;display:none"')
+  .replace(/159140/g, '990929');
 $done({ body });
