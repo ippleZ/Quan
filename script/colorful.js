62e695608862e25ccf028d55c1be883011f0d2c7
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-03 22:31:19 +0800

    Update colorful.js

--- script/colorful.js
+++ script/colorful.js
@@ -3,69 +3,90 @@ let url = $request.url;
 let obj = JSON.parse(body);
 
 if (url.indexOf('/banners') !== -1) {
-  obj.data = [
-    {
-      "title" : "",
-      "banner_type" : "",
-      "url" : "",
-      "avatar" : "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/banner1.jpg"
-    },
-    {
-      "title" : "",
-      "banner_type" : "",
-      "url" : "",
-      "avatar" : "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/banner2.jpg"
-    },
-    {
-      "title" : "",
-      "banner_type" : "",
-      "url" : "",
-      "avatar" : "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/banner3.jpg"
-    }
-  ];
-  body = JSON.stringify(obj);
+	obj.data = [{
+			"title": "",
+			"banner_type": "",
+			"url": "",
+			"avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/banner1.jpg"
+		},
+		{
+			"title": "",
+			"banner_type": "",
+			"url": "",
+			"avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/banner2.jpg"
+		},
+		{
+			"title": "",
+			"banner_type": "",
+			"url": "",
+			"avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/banner3.jpg"
+		}
+	];
+	body = JSON.stringify(obj);
+}
+
+if (url.indexOf('/features') !== -1) {
+	obj.data = [{
+			"title": "",
+			"url": "",
+			"feature_type": "",
+			"avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/icon1.jpg"
+		},
+		{
+			"title": "",
+			"url": "",
+			"feature_type": "",
+			"avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/icon2.jpg"
+		},
+		{
+			"title": "",
+			"url": "",
+			"feature_type": "",
+			"avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/icon3.jpg"
+		}
+	];
+	body = JSON.stringify(obj);
 }
 
 if (url.indexOf('/feeds') !== -1) {
-  obj.data = [
-    {
-      "avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/feeds1.jpg",
-      "visits": 520,
-      "author": "十点半睡觉",
-      "author_avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/author.png",
-      "category_times_text": "人查看",
-      "title": "十点半睡觉",
-      "category_name": "文章",
-      "feed_type": "article",
-      "feed_id": "feeds1",
-      "url": ""
-    },
-    {
-      "avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/feeds2.jpg",
-      "visits": 520,
-      "author": "十点半睡觉",
-      "author_avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/author.png",
-      "category_times_text": "人查看",
-      "title": "十点半睡觉",
-      "category_name": "文章",
-      "feed_type": "article",
-      "feed_id": "feeds1",
-      "url": ""
-    },
-    {
-      "avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/feeds3.jpg",
-      "visits": 520,
-      "author": "十点半睡觉",
-      "author_avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/author.png",
-      "category_times_text": "人查看",
-      "title": "十点半睡觉",
-      "category_name": "文章",
-      "feed_type": "article",
-      "feed_id": "feeds1",
-      "url": ""
-    }
-  ];
-  body = JSON.stringify(obj);
+	obj.data = [{
+			"avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/feeds1.jpg",
+			"visits": 520,
+			"author": "十点半睡觉",
+			"author_avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/author.png",
+			"category_times_text": "人查看",
+			"title": "十点半睡觉",
+			"category_name": "文章",
+			"feed_type": "article",
+			"feed_id": "feeds1",
+			"url": ""
+		},
+		{
+			"avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/feeds2.jpg",
+			"visits": 520,
+			"author": "十点半睡觉",
+			"author_avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/author.png",
+			"category_times_text": "人查看",
+			"title": "十点半睡觉",
+			"category_name": "文章",
+			"feed_type": "article",
+			"feed_id": "feeds1",
+			"url": ""
+		},
+		{
+			"avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/feeds3.jpg",
+			"visits": 520,
+			"author": "十点半睡觉",
+			"author_avatar": "https://cdn.jsdelivr.net/gh/ippleZ/iCloud/author.png",
+			"category_times_text": "人查看",
+			"title": "十点半睡觉",
+			"category_name": "文章",
+			"feed_type": "article",
+			"feed_id": "feeds1",
+			"url": ""
+		}
+	];
+	body = JSON.stringify(obj);
 }
 
-$done({ body });
+$done({body});
