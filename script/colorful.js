c6b6f8bebdcba718b7282c2271c07f19c9550253
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-03 20:56:28 +0800

    Update colorful.js

--- script/colorful.js
+++ script/colorful.js
@@ -3,10 +3,10 @@ let img = "";
 if (url.indexOf('/login_by_code') != -1) {
     let obj = {
         "data": {
-            "title": true,
-            "banner_type": Token,
-            "url": Token,
-            "avatar": Token
+            "title": 标题,
+            "banner_type": ,
+            "url": ,
+            "avatar": https://pss.bdstatic.com/static/superman/img/logo/logo_white-d0c9fe2af5.png
         },
     }
     body = JSON.stringify(obj);
