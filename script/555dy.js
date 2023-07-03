cac3a2ef8d740863b9be14dda30d7f1f222f12fa
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-03 10:02:55 +0800

    Update 555dy.js

--- script/555dy.js
+++ script/555dy.js
@@ -1,2 +1,3 @@
-var body = $response.body.replace(/<img id="([^"]+)" referrerpolicy="no-referrer" src="([^"]+)" width="([^"]+)">/g, '<img id="#" referrerpolicy="no-referrer" src="#" width="0%">');
+var body = $response.body
+  .replace(/<img id="([^"]+)" referrerpolicy="no-referrer" src="([^"]+)" width="([^"]+)">/g, '<img id="#" referrerpolicy="no-referrer" src="#" width="0%">');
 $done({ body: body });
