a2b9fa376b06f2ee14aaccb5f13b6219da7a929e
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-03 20:40:52 +0800

    Update colorful.js

--- script/colorful.js
+++ script/colorful.js
@@ -1,13 +1,14 @@
 var obj = JSON.parse($response.body);
 let img = "";
-if (url.indexOf('/user') != -1) {
-let obj = {
+if (url.indexOf('/login_by_code') != -1) {
+    let obj = {
         "status": "ok",
         "result": {
             "is_phone_verified": true,
             "token": Token
         },
         "rc": 0
+    }
     body = JSON.stringify(obj);
 }
 $done({body});
