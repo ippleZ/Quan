d50edb0736f48674e32f2327320e6038484cbc2d
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-03 21:13:16 +0800

    Update colorful.js

--- script/colorful.js
+++ script/colorful.js
@@ -9,4 +9,13 @@ if (url.indexOf('/banner') !== -1) {
   };
 body = JSON.stringify(obj);
 }
+if (url.indexOf('/banner') !== -1) {
+  obj.data = {
+    "title": "风场图",
+    "banner_type": "",
+    "url": "cy://button_moremap_wind",
+    "avatar": "https://cdn-w.caiyunapp.com/p/app/operation/prod/banner/6467500d38c25ada83ded8ae/d95edb5f9b64da0462cc506eb70a4c40.jpg"
+  };
+body = JSON.stringify(obj);
+}
 $done({ body });
