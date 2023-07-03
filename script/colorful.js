3e220e615b807ef9ee6c1c8622e0acfb508ec029
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-03 20:36:05 +0800

    Update colorful.js

--- script/colorful.js
+++ script/colorful.js
@@ -1,6 +1,6 @@
 var obj = JSON.parse($response.body);
 let img = "";
-if (url.indexOf('/user') != -1 || url.indexOf('/visitors') != -1) {
+if (url.indexOf('/user') != -1) {
     obj.result.token = Token;
     body = JSON.stringify(obj);
 }
