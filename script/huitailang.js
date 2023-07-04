b96776017a745f836ce90811398fa6adb7b992c8
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-04 20:36:46 +0800

    Update huitailang.js

--- script/huitailang.js
+++ script/huitailang.js
@@ -1,8 +1,8 @@
 var modifiedBody = $response.body.replace(/<div\s+class="([^"]*\s+)?ads_w(\s+[^"]*)?"[^>]*>/g, function(match) {
     if (match.includes('style="')) {
-        return match.replace('style="', 'style="display: none; ');
+        return match.replace('style="', 'style="display: none !important; ');
     } else {
-        return match.replace('<div', '<div style="display: none;"');
+        return match.replace('<div', '<div style="display: none !important;"');
     }
 });
 
