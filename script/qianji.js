703965a2c2e0b638a85127d20b8e96191c46223f
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-06 09:58:07 +0800

    Update qianji.js

--- script/qianji.js
+++ script/qianji.js
@@ -1,5 +1,5 @@
 let obj = JSON.parse($response.body);
-obj.data.userinfo.vipend = 9919990929;
-obj.data.userinfo.vipstart = 1119990929;
-obj.data.userinfo.viptype = 100;
+obj.data.config.userinfo.vipend = 9919990929;
+obj.data.config.userinfo.vipstart = 1119990929;
+obj.data.config.userinfo.viptype = 100;
 $done({ body: JSON.stringify(obj) });
