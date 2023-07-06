898e6671b2ba3f248afd5cfd736ba21fea282744
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-06 11:53:41 +0800

    Delete qianji.js

--- script/qianji.js
+++
@@ -1,5 +0,0 @@
-let obj = JSON.parse($response.body);
-obj.data.config.userinfo.vipend = 9919990929;
-obj.data.config.userinfo.vipstart = 1119990929;
-obj.data.config.userinfo.viptype = 100;
-$done({ body: JSON.stringify(obj) });
