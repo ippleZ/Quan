0bdbe87decd675173fd9177308b442c6fc042827
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-23 21:35:09 +0800

    Update picooc.js

--- script/picooc.js
+++ script/picooc.js
@@ -1,1 +1,15 @@
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
 
+if (url.includes("/function/functionMsg")) {
+  // 我的
+  if (obj.resp) {
+    obj.resp = obj.resp.filter(
+      (i) =>
+      i.groupType === "1" ||
+      i.groupType === "4"
+    );
+  }
+}
+$done({ body: JSON.stringify(obj) });
