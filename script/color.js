f235f83bc6ae3ec0b240468f8f624d3c3ab64b7b
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-07-24 11:10:30 +0800

    Update color.js

--- script/color.js
+++ script/color.js
@@ -1,39 +1,37 @@
 var obj = JSON.parse($response.body);
-const vip = '/vip_info';
-const adhf = '/activity';
-const user = '(user|visitors)';
+var url = $request.url;
 
-if ($request.url.indexOf(vip) != -1){
+if (url.includes('/vip_info')) {
     obj.vip = {
-    "expires_time" : "4092599349",
-    "is_auto_renewal" : true
-  };
+        "expires_time": "4092599349",
+        "is_auto_renewal": true
+    };
     obj.svip = {
-    "expires_time" : "4092599349",
-    "is_auto_renewal" : true
-  };
+        "expires_time": "4092599349",
+        "is_auto_renewal": true
+    };
 }
 
-if ($request.url.indexOf(adhf) != -1){
+if (url.includes('/activity')) {
     obj.activities = [];
 }
 
-if ($request.url.indexOf('user') != -1){
+if (url.includes('user') || url.includes('visitors')) {
     obj.result.ranking_above = 99;
     obj.result.is_vip = true;
     obj.result.vip_expired_at = 4092599349;
     obj.result.svip_given = 9999;
     obj.result.is_xy_vip = true;
-    obj.result.xy_svip_expire = 4092599349; 
+    obj.result.xy_svip_expire = 4092599349;
     obj.result.wt.vip = {
-        "auto_renewal_type" : "",
-        "expired_at" : 0,
-        "enabled" : true,
-        "svip_apple_expired_at" : 4092599349,
-        "is_auto_renewal" : true,
-        "svip_expired_at" : 4092599349,
-        "svip_auto_renewal_type" : ""
-      };
+        "auto_renewal_type": "",
+        "expired_at": 0,
+        "enabled": true,
+        "svip_apple_expired_at": 4092599349,
+        "is_auto_renewal": true,
+        "svip_expired_at": 4092599349,
+        "svip_auto_renewal_type": ""
+    };
     obj.result.wt.svip_given = 9999;
     obj.result.wt.ranking_above = 99;
     obj.result.is_phone_verified = true;
@@ -46,4 +44,4 @@ if ($request.url.indexOf('user') != -1){
     obj.result.vip_type = "s";
 }
 
-$done({ body: JSON.stringify(obj)});
+$done({ body: JSON.stringify(obj) });
