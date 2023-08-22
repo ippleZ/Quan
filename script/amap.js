4272b4ab84e58b88536953532bb58c18265635b5
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-08-22 14:50:34 +0800

    Update amap.js

--- script/amap.js
+++ script/amap.js
@@ -205,7 +205,7 @@ if (url.includes("/faas/amap-navigation/main-page")) {
     // "floor_guide_second", // 楼层导览
     // "footer_logo",
     // "foreclosurehouse",
-    "gallery_info", // 现场照片
+    "galleryInfo", // 现场照片
     // "ggc_entry",
     // "hkfMiniPortal", // 订票页面 飞机 火车 汽车
     "horizontalGoodsShelf",
