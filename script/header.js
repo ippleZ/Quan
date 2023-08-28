f4d3397a540c1eeabe4096f417de55400639b3f4
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-08-28 16:21:30 +0800

    Delete header.js

--- script/header.js
+++
@@ -1,9 +0,0 @@
-const url = $request.url;
-const header = $request.headers;
-let ua = header["User-Agent"] || header["user-agent"];
-
-if (url.includes("/amdc/mobileDispatch") && ua.includes("AMapiPhone")) {
-  $done({ status: "HTTP/1.1 404 Not Found" });
-} else {
-  $done({});
-}
