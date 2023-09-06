7377a483a9a649ef3bbe4a6d3e061fc5b2c6b6fe
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-09-06 14:09:58 +0800

    Update header.js

--- script/header.js
+++ script/header.js
@@ -1,5 +1,3 @@
-// 2023-08-24 12:45
-
 const url = $request.url;
 const header = $request.headers;
 const isQuanX = typeof $task !== "undefined";
@@ -8,9 +6,7 @@ let ua = header["User-Agent"] || header["user-agent"];
 if (url.includes("/amdc/mobileDispatch")) {
   if (
     ua.includes("AMapiPhone") || // 高德地图
-    ua.includes("Alibaba") || // 阿里巴巴
-    ua.includes("Cainiao4iPhone") || // 菜鸟
-    ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C") // 飞猪旅行
+    ua.includes("Alibaba") // 阿里巴巴
   ) {
     if (isQuanX) {
       $done({ status: "HTTP/1.1 404 Not Found" });
