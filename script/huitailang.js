23f46cfea4f5e89f245f2c41ebe06cd8e5e4e689
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-09-19 10:17:33 +0800

    Delete script/huitailang.js

--- script/huitailang.js
+++
@@ -1,9 +0,0 @@
-var modifiedBody = $response.body.replace(/<div\s+class="([^"]*\s+)?ads_w(\s+[^"]*)?"[^>]*>/g, function(match) {
-    if (match.includes('style="')) {
-        return match.replace('style="', 'style="display: none !important; ');
-    } else {
-        return match.replace('<div', '<div style="display: none !important;"');
-    }
-});
-
-$done({ body: modifiedBody });
