7919e242b4847e08368b64c7b4cdab7eed27000f
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-19 10:09:58 +0800

    Update video.js

--- script/video.js
+++ script/video.js
@@ -1,7 +1,10 @@
 const url = $request.url;
-if (!$response.body) $done({});
-let body = $response.body
+let body = $response.body;
+
+if (url.includes("waipian29.com")) {
   .replace(/id="player_pic"/g, 'id="player_pic" style="width: 0%"')
   .replace(/<div class="player-rm rm-two rm-list">/g, '<div class="player-rm rm-two rm-list" style="width: 0%">')
   .replace(/class="player-recommend-float"/g, 'class="player-recommend-float" style="width: 0%"');
+}
+
 $done({ body: body });
