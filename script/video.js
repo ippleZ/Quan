382c4c9d87ae03cce886cffc6edde7a998a9b453
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-19 10:02:40 +0800

    Update video.js

--- script/video.js
+++ script/video.js
@@ -1,4 +1,5 @@
 var body = $response.body
+  .replace(/id="player_pic"/g, 'id="player_pic" style="width: 0%"')
   .replace(/<div class="player-rm rm-two rm-list">/g, '<div class="player-rm rm-two rm-list" style="width: 0%">')
   .replace(/class="player-recommend-float"/g, 'class="player-recommend-float" style="width: 0%"');
 $done({ body: body });
