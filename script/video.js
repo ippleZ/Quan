20a13086e0212433d6f269479165156604b9845e
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-19 10:07:17 +0800

    Update video.js

--- script/video.js
+++ script/video.js
@@ -1,6 +1,6 @@
 const url = $request.url;
 if (!$response.body) $done({});
-let obj = $response.body
+let body = $response.body
   .replace(/id="player_pic"/g, 'id="player_pic" style="width: 0%"')
   .replace(/<div class="player-rm rm-two rm-list">/g, '<div class="player-rm rm-two rm-list" style="width: 0%">')
   .replace(/class="player-recommend-float"/g, 'class="player-recommend-float" style="width: 0%"');
