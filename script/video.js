3de0079f659fc03b75f4a5bab7af95324b9e4eee
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-19 10:14:39 +0800

    Update video.js

--- script/video.js
+++ script/video.js
@@ -6,7 +6,9 @@ if (url.includes("waipian29.com")) {
   .replace(/<div class="player-rm rm-two rm-list">/g, '<div class="player-rm rm-two rm-list" style="width: 0%">')
   .replace(/class="player-recommend-float"/g, 'class="player-recommend-float" style="width: 0%"');
 } else if (url.includes("dyxs29.com")) {
-  
+  .replace(/id="player_pic"/g, 'id="player_pic" style="width: 0%"')
+  .replace(/<div class="player-rm rm-two rm-list">/g, '<div class="player-rm rm-two rm-list" style="width: 0%">')
+  .replace(/class="player-recommend-float"/g, 'class="player-recommend-float" style="width: 0%"');
 }
 
 $done({ body: body });
