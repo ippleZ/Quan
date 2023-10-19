998f38f73dd6aa90e7e5b6074462d33fac74e301
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-19 10:16:07 +0800

    Update video.js

--- script/video.js
+++ script/video.js
@@ -1,11 +1,11 @@
 const url = $request.url;
 let body = $response.body;
 
-if (url.includes("waipian29.com")) {
+if (url.includes("waipian")) {
   .replace(/id="player_pic"/g, 'id="player_pic" style="width: 0%"')
   .replace(/<div class="player-rm rm-two rm-list">/g, '<div class="player-rm rm-two rm-list" style="width: 0%">')
   .replace(/class="player-recommend-float"/g, 'class="player-recommend-float" style="width: 0%"');
-} else if (url.includes("dyxs29.com")) {
+} else if (url.includes("dyxs")) {
   .replace(/id="player_pic"/g, 'id="player_pic" style="width: 0%"')
   .replace(/<div class="player-rm rm-two rm-list">/g, '<div class="player-rm rm-two rm-list" style="width: 0%">')
   .replace(/class="player-recommend-float"/g, 'class="player-recommend-float" style="width: 0%"');
