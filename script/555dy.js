81c321f2647305c3c8b18909cfc0966ca10b79e2
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-21 12:00:49 +0800

    Delete script/555dy.js

--- script/555dy.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-  .replace(/<img id="([^"]+)" referrerpolicy="no-referrer" src="([^"]+)" width="([^"]+)">/g, '<img id="#" referrerpolicy="no-referrer" src="#" width="0%">');
-$done({ body: body });
