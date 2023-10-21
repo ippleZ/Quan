dd96cdebe2f01b490f4454163f2c4c73b61fbcc6
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-21 11:46:47 +0800

    更新 video.js

--- script/video.js
+++ script/video.js
@@ -2,13 +2,17 @@ const url = $request.url;
 let body = $response.body;
 
 if (url.includes("waipian")) {
-  .replace(/id="player_pic"/g, 'style="display:none"')
-  .replace(/class="player-rm rm-two rm-list"/g, style="display:none"')
-  .replace(/class="player-recommend-float"/g, style="display:none"');
+  body = body
+    .replace(/id="player_pic"/g, 'style="display:none"')
+    .replace(/class="player-rm rm-two rm-list"/g, 'style="display:none"')
+    .replace(/class="player-recommend-float"/g, 'style="display:none"');
 } else if (url.includes("dyxs")) {
-  .replace(/id="player_pic"/g, style="display:none"')
-  .replace(/class="player-rm rm-two rm-list"/g, style="display:none"')
-  .replace(/class="player-recommend-float"/g, style="display:none"');
+  body = body
+    .replace(/id="player_pic"/g, 'style="display:none"')
+    .replace(/class="player-rm rm-two rm-list"/g, 'style="display:none"')
+    .replace(/class="player-recommend-float"/g, 'style="display:none"');
 }
 
-$done({ body: body });
+$done({
+  body: body
+});
