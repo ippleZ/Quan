aeacd441ffdaf7d4bf08c314b008774b9969d3a3
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-21 11:35:10 +0800

    Update video.js

--- script/video.js
+++ script/video.js
@@ -2,13 +2,13 @@ const url = $request.url;
 let body = $response.body;
 
 if (url.includes("waipian")) {
-  .replace(/id="player_pic"/g, 'style="width: 0%"')
-  .replace(/class="player-rm rm-two rm-list"/g, 'style="width: 0%"')
-  .replace(/class="player-recommend-float"/g, 'style="width: 0%"');
+  .replace(/id="player_pic"/g, 'style="display:none"')
+  .replace(/class="player-rm rm-two rm-list"/g, style="display:none"')
+  .replace(/class="player-recommend-float"/g, style="display:none"');
 } else if (url.includes("dyxs")) {
-  .replace(/id="player_pic"/g, 'style="width: 0%"')
-  .replace(/class="player-rm rm-two rm-list"/g, 'style="width: 0%"')
-  .replace(/class="player-recommend-float"/g, 'style="width: 0%"');
+  .replace(/id="player_pic"/g, style="display:none"')
+  .replace(/class="player-rm rm-two rm-list"/g, style="display:none"')
+  .replace(/class="player-recommend-float"/g, style="display:none"');
 }
 
 $done({ body: body });
