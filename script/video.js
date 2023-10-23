a0c20b72b8f6bfb5457e049ea0ac0253c18266a3
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-23 18:08:34 +0800

    Update video.js

--- script/video.js
+++ script/video.js
@@ -1,5 +1,5 @@
 var body = $response.body;
-    .replace(/id="player_pic"/g, 'style="display:none"')
+    .replace(/class="video-info-items"/g, 'style="display:none"')
     .replace(/class="player-rm rm-two rm-list"/g, 'style="display:none"')
     .replace(/class="player-recommend-float"/g, 'style="display:none"');
 }
