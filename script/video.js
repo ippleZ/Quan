c7222e56edbb014ed6c6179019fe4082d9bcb5fc
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-23 08:55:23 +0800

    Update video.js

--- script/video.js
+++ script/video.js
@@ -1,18 +1,8 @@
-const url = $request.url;
-let body = $response.body;
-
-if (url.includes("waipian")) {
-  body = body
-    .replace(/id="player_pic"/g, 'style="display:none"')
-    .replace(/class="player-rm rm-two rm-list"/g, 'style="display:none"')
-    .replace(/class="player-recommend-float"/g, 'style="display:none"');
-} else if (url.includes("dyxs")) {
-  body = body
+var body = $response.body;
     .replace(/id="player_pic"/g, 'style="display:none"')
     .replace(/class="player-rm rm-two rm-list"/g, 'style="display:none"')
     .replace(/class="player-recommend-float"/g, 'style="display:none"');
 }
-
 $done({
   body: body
 });
