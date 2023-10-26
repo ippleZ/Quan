ad9b946ba7ea8e734c6547f151e708ea515dd4e0
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-26 10:27:21 +0800

    Update video.js

--- script/video.js
+++ script/video.js
@@ -1,1 +1,3 @@
-
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.gitmirror.com/ippleZ/Quan/master/style/waipian.css" type="text/css">');
+$done({ body });
