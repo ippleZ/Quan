674d777715f8c7c7637f0b73b314d0c2ed131bf4
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-10-26 15:37:49 +0800

    Update video.js

--- script/video.js
+++ script/video.js
@@ -1,3 +1,10 @@
-var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.gitmirror.com/ippleZ/Quan/master/style/waipian.css" type="text/css">');
+var url = $request.url;
+var body = $response.body;
+
+if (url.includes("waipian")) {
+  body = body.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.gitmirror.com/ippleZ/Quan/master/style/waipian.css" type="text/css">');
+} else if (url.includes("bdys")) {
+  body = body.replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.gitmirror.com/ippleZ/Quan/master/style/bdys.css" type="text/css">');
+}
+
 $done({ body });
