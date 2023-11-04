03825ecbaed18b12a091c7f72eea206e922813cc
橙子 <135012529+ippleZ@users.noreply.github.com>
2023-11-04 18:03:29 +0800

    Update color.js

--- script/color.js
+++ script/color.js
@@ -2,47 +2,34 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes('/vip_info')) {
-    obj.vip = {
-        "expires_time": "4092599349",
-        "is_auto_renewal": true
-    };
-    obj.svip = {
-        "expires_time": "4092599349",
-        "is_auto_renewal": true
-    };
-}
-
-if (url.includes('/activity')) {
-    obj.activities = [];
-}
-
-if (url.includes('user') || url.includes('visitors')) {
-    obj.result.ranking_above = 99;
-    obj.result.is_vip = true;
-    obj.result.vip_expired_at = 4092599349;
-    obj.result.svip_given = 9999;
-    obj.result.is_xy_vip = true;
-    obj.result.xy_svip_expire = 4092599349;
-    obj.result.wt.vip = {
-        "auto_renewal_type": "",
-        "expired_at": 0,
-        "enabled": true,
-        "svip_apple_expired_at": 4092599349,
-        "is_auto_renewal": true,
-        "svip_expired_at": 4092599349,
-        "svip_auto_renewal_type": ""
-    };
-    obj.result.wt.svip_given = 9999;
-    obj.result.wt.ranking_above = 99;
+if (url.includes("/v1/vip_info")) {
+  if (obj.vip) {
+    obj.vip.expires_time = "4030000000";
+  }
+  if (obj.svip) {
+    obj.svip.expires_time = "4030000000";
+  }
+} else if (url.includes("/v2/user")) {
+  if (obj.result) {
+    obj.result.svip_given = 730;
     obj.result.is_phone_verified = true;
-    obj.result.phone_num = "13145200000";
-    obj.result.vip_take_effect = 1;
+    obj.result.is_xy_vip = true;
+    obj.result.vip_expired_at = 4030000000.16;
+    obj.result.is_vip = true;
+    obj.result.xy_svip_expire = 4030000000.16;
+    if (obj.result.wt) {
+      if (obj.result.wt.vip) {
+        obj.result.wt.vip.enabled = true;
+        obj.result.wt.vip.expired_at = 4030000000.16;
+        obj.result.wt.vip.svip_expired_at = 4030000000.16;
+      }
+      obj.result.wt.svip_given = 730;
+    }
     obj.result.is_primary = true;
-    obj.result.xy_vip_expire = 4092599349;
-    obj.result.svip_expired_at = 4092599349;
-    obj.result.svip_take_effect = 1;
+    obj.result.xy_vip_expire = 4030000000.16;
+    obj.result.svip_expired_at = 4030000000.16;
     obj.result.vip_type = "s";
+  }
 }
 
 $done({ body: JSON.stringify(obj) });
