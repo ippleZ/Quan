c77196e90219c069b30d9f579bb1b1e5485985e8
橙子 <135012529+ippleZ@users.noreply.github.com>
2024-05-08 11:32:55 +0800

    Update 22306.js

--- script/22306.js
+++ script/22306.js
@@ -1,27 +1,26 @@
 const url = $request.url;
 const isQuanX = typeof $task !== "undefined";
-
-if (url.includes("/ad.12306.cn/ad/ser/getAdList")) {
-	let body = "";
+let body = "";
 let obj = JSON.parse($request.body);
-	if (obj.placementNo === "0007") {
-		body =
-			'{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
-	} else if (obj.placementNo === "G0054") {
-		body = '{"code":"00","materialsList":[]}';
-	} else {
-		body = '{"code":"00","message":"无广告返回"}';
-	}
+if (url.includes("/ad.12306.cn/ad/ser/getAdList")) {
+  if (obj.placementNo === "0007") {
+    body =
+      '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+  } else if (obj.placementNo === "G0054") {
+    body = '{"code":"00","materialsList":[]}';
+  } else {
+    body = '{"code":"00","message":"无广告返回"}';
+  }
 
-	if (isQuanX) {
-		$done({
-			body
-		});
-	} else {
-		$done({
-			response: {
-				body
-			}
-		});
-	}
+  if (isQuanX) {
+    $done({
+      body
+    });
+  } else {
+    $done({
+      response: {
+        body
+      }
+    });
+  }
 }
