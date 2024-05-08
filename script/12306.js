37c3710266623aa5c5e7031d3b9a53817726a51d
橙子 <135012529+ippleZ@users.noreply.github.com>
2024-05-08 10:09:45 +0800

    Update 12306.js

--- script/12306.js
+++ script/12306.js
@@ -35,27 +35,4 @@ if(url.includes("/mobile.12306.cn/otsmobile/app/mgs/")) {
       $done({});
     }
   }
-} else if(url.includes("/ad.12306.cn/ad/ser/getAdList")) {
-  let body = "";
-  let obj = JSON.parse($request.body);
-  if(obj.placementNo === "0007") {
-    body =
-      '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
-  } else if(obj.placementNo === "G0054") {
-    body = '{"code":"00","materialsList":[]}';
-  } else {
-    body = '{"code":"00","message":"无广告返回"}';
-  }
-
-  if(isQuanX) {
-    $done({
-      body
-    });
-  } else {
-    $done({
-      response: {
-        body
-      }
-    });
-  }
 }
