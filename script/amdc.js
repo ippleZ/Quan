12e06ad4bfb601b878d31abccbf820dedfe08121
橙子 <135012529+ippleZ@users.noreply.github.com>
2024-05-09 14:32:15 +0800

    Update amdc.js

--- script/amdc.js
+++ script/amdc.js
@@ -1,11 +1,13 @@
 const url = $request.url;
 const ua = header["User-Agent"] || header["user-agent"];
 
-if (url.includes("/amdc/mobileDispatch")) {
-  if (
+if(url.includes("/amdc/mobileDispatch")) {
+  if(
     ua.includes("AMapiPhone")
   ) {
-    $done({ status: "HTTP/1.1 404 Not Found" });
+    $done({
+      status: "HTTP/1.1 404 Not Found"
+    });
   } else {
     $done({});
   }
