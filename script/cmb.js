27ef69d141446dc85626f7267dc8e3a4a9adb87b
橙子 <135012529+ippleZ@users.noreply.github.com>
2024-07-18 16:05:36 +0800

    Update cmb.js

--- script/cmb.js
+++ script/cmb.js
@@ -1,15 +1,21 @@
-let body = $response.body;
-let obj = JSON.parse(body);
+// 解析JSON数据
+let obj = JSON.parse($response.body);
 
+// 遍历ernInfo数组
 obj.bizResult.data.ernInfo.forEach(item => {
-    let ernMon = parseFloat(item.ernMon);
-    if (ernMon > 0) {
-        ernMon *= 2;
-    } else {
-        ernMon /= 2;
-    }
-    item.ernMon = ernMon.toFixed(2);
+  // 将ernMon转换为数字
+  let ernMon = parseFloat(item.ernMon);
+  
+  // 根据条件处理ernMon
+  if (ernMon > 0) {
+    ernMon *= 2;
+  } else if (ernMon < 0) {
+    ernMon /= 2;
+  }
+  
+  // 四舍五入到两位小数
+  item.ernMon = ernMon.toFixed(2);
 });
 
-body = JSON.stringify(obj);
-$done({body});
+// 将修改后的对象转回JSON字符串
+$done({body: JSON.stringify(obj)});
