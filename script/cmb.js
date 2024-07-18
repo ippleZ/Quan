b4c59a6625a6a9644d152564795ea8f4b0d28123
橙子 <135012529+ippleZ@users.noreply.github.com>
2024-07-18 16:53:54 +0800

    Update cmb.js

--- script/cmb.js
+++ script/cmb.js
@@ -1,33 +1,18 @@
 let obj = JSON.parse($response.body);
 
 function processValue(value) {
-  let num = parseFloat(value);
-  if (num > 0) {
-    num *= 2;
-  } else if (num < 0) {
-    num /= 2;
-  }
-  return num.toFixed(2);
+    let num = parseFloat(value);
+    return num > 0 ? (num * 2).toFixed(2) : (num / 2).toFixed(2);
 }
 
-if (obj.bizResult && obj.bizResult.data) {
-  if (obj.bizResult.data.ernInfo) {
-    obj.bizResult.data.ernInfo.forEach(item => {
-      if (item.ernMon) {
-        item.ernMon = processValue(item.ernMon);
-      }
-    });
-  }
-  
-  if (obj.bizResult.data.weekList) {
+if (obj.bizResult?.data?.weekList) {
     obj.bizResult.data.weekList.forEach(week => {
-      week.sevenDay.forEach(day => {
-        if (day.income) {
-          day.income = processValue(day.income);
-        }
-      });
+        week.sevenDay.forEach(day => {
+            if (day.income) {
+                day.income = processValue(day.income);
+            }
+        });
     });
-  }
 }
 
 $done({body: JSON.stringify(obj)});
