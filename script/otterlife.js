57d20e4f2d6ad513820b22d4d89ffa7cf4484b01
橙子 <135012529+ippleZ@users.noreply.github.com>
2024-08-06 10:03:07 +0800

    Create otterlife.js

---
+++ script/otterlife.js
@@ -0,0 +1,10 @@
+var body = JSON.parse($response.body);
+
+body.data = {
+  ...body.data,
+  "vipType" : "lifetime",
+  "vipDeadline" : "2099-09-09T09:09:09.000Z",
+  "isVip" : true
+};
+
+$done({body : JSON.stringify(body)});
